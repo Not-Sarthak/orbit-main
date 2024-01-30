d62a0deb44ed865cc62e14a309bb01390bdef987
Sarthak <notsarthakshah@gmail.com>
2024-01-30 16:01:13 +0530

    border animation

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import services from "../../assets/services.svg"
 
+import '../../index.css'
+
 const Services = () => {
   return (
     <div>
@@ -11,26 +13,26 @@ const Services = () => {
         </div>
       </h1>
     <div className='flex justify-center items-center p-4'>
-      <div className="grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-        {/* Left */}
-        <div className="relative col-span-1 overflow-hidden rounded-xl bg-transparent">
-          <div className="flex items-center justify-center">
-            <img src={services} alt="services" />
-          </div>
-        </div>
-        {/* Right */}
-        <div className="relative col-span-1 rounded-xl bg-white md:col-span-2 bg-transparent">
-          <div className="flex h-full items-center justify-center">
-            <div className='grid grid-cols-2 md:gap-56 md:p-0 p-20 pb-0 gap-28'>
-              <div>Service 1</div>
-              <div>Service 2</div>
-              <div>Service 3</div>
-              <div>Service 4</div>
-            </div>
-          </div>
-        </div>
-
+    <div className="grid w-full max-w-screen-xl animate-fade-up bg-white border border-gray-200 p-8 rounded-2xl grid-cols-1 gap-5 px-5 md:grid-cols-3 custom-shadow">
+  {/* Left */}
+  <div className="relative col-span-1 overflow-hidden rounded-xl bg-transparent">
+    <div className="flex items-center justify-center">
+      <img src={services} alt="services" />
+    </div>
+  </div>
+  {/* Right */}
+  <div className="relative col-span-1 rounded-xl bg-white md:col-span-2 bg-transparent">
+    <div className="flex h-full items-center justify-center">
+      <div className='grid grid-cols-2 md:gap-56 md:p-0 pt-16 pb-0 gap-28'>
+        <div>Service 1</div>
+        <div>Service 2</div>
+        <div>Service 3</div>
+        <div>Service 4</div>
       </div>
+    </div>
+  </div>
+</div>
+
     </div>
     </div>
   );
