42f9716765ce0f0eb3e458df02ab232c6f9e67e1
Not-Sarthak <notsarthakshah@gmail.com>
2024-02-02 15:57:18 +0530

    demo button

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -21,9 +21,9 @@ const Footer = () => {
                 <div class="grid gap-8 items-start justify-center">
                     <div class="relative group">
                     <div class="absolute -inset-0.5 bg-gradient-to-r from-blue-400 to-purple-300 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-                    <button class="relative px-7 py-4 bg-white hover:bg-zinc-100 rounded-full leading-none flex items-center divide-x divide-gray-600">
+                    <button class="relative w-48 px-7 py-4 bg-white hover:bg-zinc-100 rounded-full leading-none flex items-center justify-center divide-x divide-gray-600">
                         <span class="flex items-center justify-center">
-                            <span class="text-black group-hover:text-black transition duration-200">Schedule a demo &rarr;</span>
+                            <span class="text-black group-hover:text-black transition duration-200">Schedule a demo</span>
                         </span>
                     </button>
                     </div>
